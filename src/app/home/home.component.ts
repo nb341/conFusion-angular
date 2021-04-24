47e96ea7f9c1a7e1cbb0d6a1fa5905e66797e155
Narindra Balkissoon <narindrab4@gmail.com>
2021-04-24 09:41:00 -0400

    RxJs Part 1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -22,9 +22,9 @@ export class HomeComponent implements OnInit {
     private promotionservice: PromotionService, private leaderservice: LeaderService) { }
 
   ngOnInit() {
-    this.dishservice.getFeaturedDish().then(dish=>this.dish=dish);
-    this.promotionservice.getFeaturedPromotion().then(promo=>this.promotion=promo);
-    this.leaderservice.getFeaturedLeader().then(leader=>this.leader=leader);
+    this.dishservice.getFeaturedDish().subscribe(dish=>this.dish=dish);
+    this.promotionservice.getFeaturedPromotion().subscribe(promo=>this.promotion=promo);
+    this.leaderservice.getFeaturedLeader().subscribe(leader=>this.leader=leader);
   }
 
 }
