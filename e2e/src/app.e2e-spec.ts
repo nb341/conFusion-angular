2972db1195dd619b69bcf4676f22d7213a7238fa
Narindra Balkissoon <narindrab4@gmail.com>
2021-04-28 11:57:48 -0400

    e2e

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,5 +1,6 @@
-import { browser, logging } from 'protractor';
+
 import { AppPage } from './app.po';
+import { browser } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -8,16 +9,32 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', async () => {
-    await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('conFusion app is running!');
+  it('should display message saying Ristorante Con Fusion', async () => {
+    page.navigateTo('/');
+    expect(await page.getParagraphText('app-root h1')).toEqual('Ristorante Con Fusion');
   });
 
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
+  it('should navigate to about us page by clicking on the link', async () => {
+   await page.navigateTo('/');
+
+    const navlink = await page.getAllElements('a').get(1);
+    navlink.click();
+
+    expect(await page.getParagraphText('h3')).toBe('About Us');
+  });
+
+  it('should enter a new comment for the first dish', async() => {
+    await page.navigateTo('/dishdetail/0');
+
+    const newAuthor = await page.getElement('input[type=text]');
+    await newAuthor.sendKeys('Test Author');
+
+    const newComment = await page.getElement('textarea');
+    await newComment.sendKeys('Test Comment');
+
+    const newSubmitButton = await page.getElement('button[type=submit]');
+    await newSubmitButton.click();
+
+    browser.pause();
   });
 });
